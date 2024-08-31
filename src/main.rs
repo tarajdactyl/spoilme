d211bc5e40c30894d51dd23617f6757d79e27a79
Tara Evans-Ellis <taraevansellis@gmail.com>
2024-08-31 19:37:10 -0400

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,40 @@ use std::env;
 
 use serenity::async_trait;
 use serenity::model::channel::{Message, MessageReference};
+use serenity::model::id::{ChannelId, MessageId};
 use serenity::model::gateway::Ready;
 use serenity::builder::{CreateAttachment, CreateMessage};
+use serenity::Result as SerenityResult;
 use serenity::prelude::*;
 
 struct Handler;
+
+impl Handler{
+
+    async fn handle_spoilme(ctx: Context, channel_id: ChannelId, message_id: MessageId) ->
+        SerenityResult<Message>{
+
+        // TODO: this errors out if there are reactions!! error about burst_colours
+        let ref_msg = ctx.http.get_message(channel_id, message_id).await?;
+        //println!("Got referenced message: {:?}\n\n",ref_msg);
+
+        //println!("Attachments to spoil:");
+        let mut attachments: Vec<CreateAttachment> = vec!();
+        for attachment in ref_msg.attachments{
+            //println!(" - {:?}", attachment.url);
+
+            let mut spoiled_attachment = CreateAttachment::url(&ctx, &attachment.url).await?;
+            if ! spoiled_attachment.filename.starts_with("SPOILER_") {
+                spoiled_attachment.filename = format!("SPOILER_{}",spoiled_attachment.filename);
+            }
+            //println!("filename: {}", spoiled_attachment.filename);
+            attachments.push(spoiled_attachment);
+
+        }
+        let builder = CreateMessage::new().content(ref_msg.content);
+        channel_id.send_files(&ctx, attachments, builder).await
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -14,45 +43,24 @@ impl EventHandler for Handler {
     //
     // Event handlers are dispatched through a threadpool, and so multiple events can be
     // dispatched simultaneously.
+
     async fn message(&self, ctx: Context, msg: Message) {
         //println!("{:?}\n\n", msg);
         if msg.content == "!spoilme" {
             // Sending a message can fail, due to a network error, an authentication error, or lack
             // of permissions to post in the channel, so log to stdout when some error happens,
             // with a description of it.
+
             match msg.message_reference{
                 Some(MessageReference{channel_id, message_id, ..}) =>{
                     if let Some(message_id) = message_id{
-
-                        // TODO: this errors out if there are reactions!! error about burst_colours
-                        let ref_msg = ctx.http.get_message(channel_id, message_id).await;
-                        //println!("Got referenced message: {:?}\n\n",ref_msg);
-
-                        if let Ok(ref_msg) = ref_msg {
-
-                            println!("Attachments to spoil:");
-                            let mut attachments: Vec<CreateAttachment> = vec!();
-                            for attachment in ref_msg.attachments{
-                                println!(" - {:?}", attachment.url);
-
-                                if let Ok(mut spoiled_attachment) = CreateAttachment::url(&ctx, &attachment.url).await{
-                                    if ! spoiled_attachment.filename.starts_with("SPOILER_") {
-                                        spoiled_attachment.filename = format!("SPOILER_{}",spoiled_attachment.filename);
-                                    }
-                                    //println!("filename: {}", spoiled_attachment.filename);
-                                    attachments.push(spoiled_attachment);
-
-                                }
-                            }
-                            let builder = CreateMessage::new().content(ref_msg.content);
-                            if let Err(why) = channel_id.send_files(&ctx, attachments, builder).await{
-                                println!("Error sending message: {why:?}");
-                            }
+                        if let Err(why) = Self::handle_spoilme(ctx, channel_id, message_id).await{
+                            println!("Failed to handle spoilme: {}", why);
                         }
                     }
                 }
                 _ => ()
-            };
+            }
         }
     }
 
