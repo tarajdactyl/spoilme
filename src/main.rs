6dc09e362f5d60e14e6e9c052df7488acf252a36
Tara Evans-Ellis <taraevansellis@gmail.com>
2024-08-31 21:23:27 -0400

    fixed formatting with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,56 @@
 use std::env;
 
 use serenity::async_trait;
-use serenity::model::channel::{Message, MessageReference};
-use serenity::model::id::{ChannelId, MessageId};
-use serenity::model::gateway::Ready;
 use serenity::builder::{CreateAttachment, CreateMessage};
-use serenity::Result as SerenityResult;
-use serenity::utils::MessageBuilder;
+use serenity::model::channel::{Message, MessageReference};
+use serenity::model::gateway::Ready;
+use serenity::model::id::{ChannelId, MessageId};
 use serenity::prelude::*;
 use serenity::utils::FormattedTimestamp;
+use serenity::utils::MessageBuilder;
+use serenity::Result as SerenityResult;
 
 struct Handler;
 
-impl Handler{
-
-    async fn handle_spoilme(ctx: Context, channel_id: ChannelId, message_id: MessageId) ->
-        SerenityResult<()>{
-
-        // TODO: this errors out if there are reactions!! error about burst_colours
+impl Handler {
+    async fn handle_spoilme(
+        ctx: Context,
+        channel_id: ChannelId,
+        message_id: MessageId,
+    ) -> SerenityResult<()> {
         let ref_msg = channel_id.message(&ctx, message_id).await?;
-        //println!("Got referenced message: {:?}\n\n",ref_msg);
 
-        let mut attachments: Vec<CreateAttachment> = vec!();
-        for attachment in &ref_msg.attachments{
+        let mut attachments: Vec<CreateAttachment> = vec![];
+        for attachment in &ref_msg.attachments {
             let mut spoiled_attachment = CreateAttachment::url(&ctx, &attachment.url).await?;
-            if ! spoiled_attachment.filename.starts_with("SPOILER_") {
-                spoiled_attachment.filename = format!("SPOILER_{}",spoiled_attachment.filename);
+            if !spoiled_attachment.filename.starts_with("SPOILER_") {
+                spoiled_attachment.filename = format!("SPOILER_{}", spoiled_attachment.filename);
             }
             attachments.push(spoiled_attachment);
-
         }
 
         let mut builder = MessageBuilder::new();
-        builder.mention(&ref_msg.author)
-            .push(format!("\n_ {}",FormattedTimestamp::from(ref_msg.timestamp)));
-        if let Some(edit_ts) = ref_msg.edited_timestamp{
-            builder.push(" (edited at ")
-            .push(FormattedTimestamp::from(edit_ts).to_string())
-            .push(")");
+        builder.mention(&ref_msg.author).push(format!(
+            "\n_ {}",
+            FormattedTimestamp::from(ref_msg.timestamp)
+        ));
+        if let Some(edit_ts) = ref_msg.edited_timestamp {
+            builder
+                .push(" (edited at ")
+                .push(FormattedTimestamp::from(edit_ts).to_string())
+                .push(")");
         }
         builder.push('_'); // end italic
         if !ref_msg.content.is_empty() {
             builder.push('\n').push_quote(&ref_msg.content);
         }
 
-        channel_id.send_files(&ctx, attachments,
-                              CreateMessage::new().content(builder.build()))
+        channel_id
+            .send_files(
+                &ctx,
+                attachments,
+                CreateMessage::new().content(builder.build()),
+            )
             .await?;
         ref_msg.delete(&ctx).await
     }
@@ -57,23 +62,21 @@ impl EventHandler for Handler {
     //
     // Event handlers are dispatched through a threadpool, and so multiple events can be
     // dispatched simultaneously.
-
     async fn message(&self, ctx: Context, msg: Message) {
-        //println!("{:?}\n\n", msg);
         if msg.content == "!spoilme" {
-            // Sending a message can fail, due to a network error, an authentication error, or lack
-            // of permissions to post in the channel, so log to stdout when some error happens,
-            // with a description of it.
-
-            match msg.message_reference{
-                Some(MessageReference{channel_id, message_id, ..}) =>{
-                    if let Some(message_id) = message_id{
-                        if let Err(why) = Self::handle_spoilme(ctx, channel_id, message_id).await{
+            match msg.message_reference {
+                Some(MessageReference {
+                    channel_id,
+                    message_id,
+                    ..
+                }) => {
+                    if let Some(message_id) = message_id {
+                        if let Err(why) = Self::handle_spoilme(ctx, channel_id, message_id).await {
                             println!("Failed to handle spoilme: {}", why);
                         }
                     }
                 }
-                _ => ()
+                _ => (),
             }
         }
     }
@@ -99,8 +102,10 @@ async fn main() {
 
     // Create a new instance of the Client, logging in as a bot. This will automatically prepend
     // your bot token with "Bot ", which is a requirement by Discord for bot users.
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
     // Finally, start a single shard, and start listening to events.
     //
